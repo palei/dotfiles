ac2a52101c536e0745890f274b006e92b139ed03
Ilkka Pale <ilkka.pale@jyu.fi>
2014-03-11 16:56:53 +0100

    a bit more logic

--- install.py
+++ install.py
@@ -1,9 +1,39 @@
 #! /usr/bin/env python
-
-import yaml
+from optparse import OptionParser
+import sys, yaml, os, shutil
 
 def main():
-    pass
+
+    config = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if not config:
+        return "Usage: ./install.py <config>"
+
+    settings = read_settings()
+
+    if config not in settings:
+        return "There's no config with that name."
+
+    src = os.path.join('configs', settings[config]['src'])
+    dst = settings[config]['dst']
+
+    if not os.path.exists(src):
+        return "Source config does not exist."
+
+    if os.path.exists(dst):
+        print "The destination file already exists. Over-write? [y/N/d] ", 
+
+    shutil.copy(src, dst)    
+
+
+
+def read_settings():
+    try:
+        stream = open('settings.yaml', 'r')
+        return yaml.load(stream)
+    except Error:
+        print "Could not parse settings.yaml. Aborting."
+        sys.exit(0)
 
 if __name__ == '__main__':
-    main()
+    print main()
